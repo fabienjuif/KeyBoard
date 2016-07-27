1a0e89f3d89a01716edf5f6c62003382f84adb09
Fabien JUIF <fabien.juif@zenika.com>
2016-07-27 18:41:19 +0200

    Testing, SCSS, and linting

--- app/Login/reducers.js
+++ app/Login/reducers.js
@@ -1,7 +1,7 @@
 export const initState = {}
 export const initAction = { type: 'UNKNOWN_ACTION' }
 
-import { LOGIN, LOGOUT, LOGIN_FAIL } from './actions'
+import { LOGIN, LOGOUT, FAIL_LOGIN } from './actions'
 
 export default (state = initState, action = initAction) => {
   switch (action.type) {
@@ -11,7 +11,7 @@ export default (state = initState, action = initAction) => {
       status: action.type.toLowerCase(),
       user: action.user,
     }
-    case LOGIN_FAIL:
+    case FAIL_LOGIN:
     case LOGOUT: return {
       ...state,
       logged: false,
