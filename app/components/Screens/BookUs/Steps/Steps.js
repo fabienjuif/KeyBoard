24415478cb7d342f7bd39130eea666426d3b81ad
Fabien JUIF <fabien.juif@zenika.com>
2016-08-07 15:34:16 +0200

    Linter

--- app/components/Screens/BookUs/Steps/Steps.js
+++ app/components/Screens/BookUs/Steps/Steps.js
@@ -6,7 +6,9 @@ import styles from './styles.scss'
 const Steps = ({ steps }) => {
   return (
     <div className={styles.steps}>
-      {steps.map((step, index) => <Round key={step} step={step} last={index === steps.length - 1} />)}
+      {steps.map((step, index) =>
+        <Round key={step} step={step} last={index === steps.length - 1} />)
+      }
     </div>
   )
 }
